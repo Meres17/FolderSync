45dae652bb3b201189b187fc62f28943ae91872b
syz <syz@mail.hamastar.com.tw>
2025-01-07 15:00:38 +0800

    重構 調整FolderService

---
+++ FolderSyncCore/FolderService.cs
@@ -0,0 +1,38 @@
+﻿namespace FolderSyncCore
+{
+    public class FolderService
+    {
+        private readonly AppSettings _appSettings;
+        private readonly FileBackup _filebackup;
+        private readonly DictionaryComparer _dictionaryComparer;
+
+
+        public FolderService(AppSettings appSettings)
+        {
+            _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
+            _filebackup = new FileBackup(appSettings);
+            _dictionaryComparer = new DictionaryComparer(appSettings);
+        }
+
+        public void Backup(string sourceDir, string targetDir)
+        {
+            var files = GetDiffFiles(sourceDir, targetDir);
+            _filebackup.Backup(files, sourceDir, targetDir);
+        }
+
+        public List<FileStatus> GetDiffFiles(string sourceDir, string targetDir)
+        {
+            return _dictionaryComparer.GetFiles(sourceDir, targetDir);
+        }
+
+        public List<FolderDTO> GetBackupFolders(string sourceDir, string targetDir)
+        {
+            return _filebackup.GetFolders(sourceDir, targetDir);
+        }
+
+        public void Restore(string backupDir, string targetDir)
+        {
+            _filebackup.Restore(backupDir, targetDir);
+        }
+    }
+}
--- FolderSyncForm/MainForm.cs
+++ FolderSyncForm/MainForm.cs
@@ -6,12 +6,19 @@ namespace FolderSyncForm
 {
     public partial class MainForm : System.Windows.Forms.Form
     {
+        private readonly FolderService _service;
+        private readonly Site _site;
+        private string _source => txtSource.Text;
+        private string _target => txtTarget.Text;
+
         public MainForm()
         {
             InitializeComponent();
-            var settings = AppSettings.Build();
-            txtSource.Text = settings.Source;
-            txtTarget.Text = settings.Target;
+            _site = new Site();
+            var appSettings = AppSettings.Build();
+            _service = new FolderService(appSettings);
+            txtSource.Text = appSettings.Source;
+            txtTarget.Text = appSettings.Target;
         }
 
         private void btnSource_Click(object sender, EventArgs e)
@@ -26,25 +33,23 @@ namespace FolderSyncForm
 
         private void btnCompare_Click(object sender, EventArgs e)
         {
-            gvDiff.DataSource = new FolderComparer(txtSource.Text, txtTarget.Text).GetDiffFiles();
+            gvDiff.DataSource = _service.GetDiffFiles(_source, _target);
         }
 
         private void btnCopy_Click(object sender, EventArgs e)
         {
-            var comparer = new FolderComparer(txtSource.Text, txtTarget.Text);
-            comparer.Backup();
+            _service.Backup(_source, _target);
             MessageBox.Show("複製成功，差異檔案已備份至本目錄底下backup/來源_目標/時間標記");
         }
 
         private void btnUpdateSite_Click(object sender, EventArgs e)
         {
-            var comparer = new FolderComparer(txtSource.Text, txtTarget.Text);
 
-            if (comparer.ExistOffline())
+            if (_site.CanClose())
             {
-                comparer.CloseSite();
-                comparer.Backup();
-                comparer.OpenSite();
+                _site.CloseSite(_target);
+                _service.Backup(_source, _target);
+                _site.OpenSite(_target);
                 MessageBox.Show("站台更新成功，差異檔案已備份至本目錄底下backup/來源_目標/時間標記");
             }
             else
@@ -55,7 +60,7 @@ namespace FolderSyncForm
 
         private void btnBackupList_Click(object sender, EventArgs e)
         {
-            gvDiff.DataSource = new FolderComparer(txtSource.Text, txtTarget.Text).GetBackupFolders();
+            gvDiff.DataSource = _service.GetBackupFolders(_source, _target);
         }
 
         private void btnRestore_Click(object sender, EventArgs e)
@@ -73,13 +78,11 @@ namespace FolderSyncForm
             }
 
             // 取得選取的 FolderDTO 物件
-            var dto = (FolderDTO)gvDiff.SelectedRows[0].DataBoundItem;
+            var backupDir = (FolderDTO)gvDiff.SelectedRows[0].DataBoundItem;
 
-            new FolderComparer(txtSource.Text, txtTarget.Text).Restore(dto.完整路徑);
+            _service.Restore(backupDir.完整路徑, _target);
 
             MessageBox.Show("還原成功");
         }
-
-
     }
 }
