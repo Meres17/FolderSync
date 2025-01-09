25a560d58fe1662bb1b12f4f04af2ccaa89eadc9
syz <syz@mail.hamastar.com.tw>
2025-01-09 09:47:47 +0800

    重構 使用備份工廠

---
+++ FolderSyncCore/BackupFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace FolderSyncCore
+{
+    public class BackupFactory
+    {
+        private readonly AppSettings _appSettings;
+
+        public BackupFactory(AppSettings appSettings)
+        {
+            _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
+        }
+
+        public string[] GetNames()
+        {
+            return new[] { "資料夾", ".NET站台" };
+        }
+
+        public IBackup Create(string name)
+        {
+            return name switch
+            {
+                "資料夾" => new FolderBackup(_appSettings),
+                ".NET站台" => new NetSiteBackup(new FolderBackup(_appSettings)),
+                _ => throw new NotSupportedException($"不支援的備份類型：{name}")
+            };
+        }
+    }
+}
--- FolderSyncForm/FolderSyncAppService.cs
+++ FolderSyncForm/FolderSyncAppService.cs
@@ -4,19 +4,23 @@ namespace FolderSyncForm
 {
     internal class FolderSyncAppService
     {
-        private readonly NetSiteBackup _netSiteBackup;
-        private readonly AppSettings _appSettings;
-        private readonly FolderBackup _filebackup;
+        private readonly BackupFactory _factory;
+        private readonly IBackup _webSite;
+        private readonly IBackup _folder;
         private readonly DictionaryComparer _dictionaryComparer;
 
         public FolderSyncAppService(AppSettings appSettings)
         {
-            _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
-            _filebackup = new FolderBackup(appSettings);
+            _factory = new BackupFactory(appSettings);
+            _folder = _factory.Create("資料夾");
+            _webSite = _factory.Create(".NET站台");
             _dictionaryComparer = new DictionaryComparer(appSettings);
-            _netSiteBackup = new NetSiteBackup(_filebackup);
         }
 
+        public string[] GetNames()
+        {
+            return _factory.GetNames();
+        }
 
         public List<FileStatus> GetDiffFiles(string sourceDir, string destDir)
         {
@@ -25,34 +29,34 @@ namespace FolderSyncForm
 
         public List<FolderDTO> GetBackupFolders(string sourceDir, string destDir)
         {
-            return _filebackup.GetFolders(sourceDir, destDir);
+            return _folder.GetFolders(sourceDir, destDir);
         }
 
-        public void RestoreSite(DataGridView gv, string dest)
+        public void RestoreSite(DataGridView gv, string destDir)
         {
             var backupDir = GetBackupDir(gv);
-            _netSiteBackup.Restore(backupDir.完整路徑, dest);
+            _webSite.Restore(backupDir.完整路徑, destDir);
         }
 
         public void Restore(DataGridView gv, string destDir)
         {
             var backupDir = GetBackupDir(gv);
-            _filebackup.Restore(backupDir.完整路徑, destDir);
+            _folder.Restore(backupDir.完整路徑, destDir);
         }
 
-        private FolderDTO GetBackupDir(DataGridView gvDiff)
+        private FolderDTO GetBackupDir(DataGridView gv)
         {
-            if (gvDiff.SelectedRows.Count == 0)
+            if (gv.SelectedRows.Count == 0)
             {
                 throw new Exception("請選擇要還原的檔案");
             }
 
-            if (gvDiff.SelectedRows.Count > 1)
+            if (gv.SelectedRows.Count > 1)
             {
                 throw new Exception("要還原的檔案只能選一個");
             }
 
-            var result = (FolderDTO)gvDiff.SelectedRows[0].DataBoundItem;
+            var result = (FolderDTO)gv.SelectedRows[0].DataBoundItem;
 
             if (result is null)
             {
@@ -66,13 +70,13 @@ namespace FolderSyncForm
         public void UpdateSite(string sourceDir, string destDir)
         {
             var files = GetDiffFiles(sourceDir, destDir);
-            _netSiteBackup.Backup(files, sourceDir, destDir);
+            _webSite.Backup(files, sourceDir, destDir);
         }
 
         public void Backup(string sourceDir, string destDir)
         {
             var files = GetDiffFiles(sourceDir, destDir);
-            _filebackup.Backup(files, sourceDir, destDir);
+            _folder.Backup(files, sourceDir, destDir);
         }
 
     }
